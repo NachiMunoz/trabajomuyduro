e9b7d631eb9e502502a650a1f5d7b61a9353705e
nachimunoz <nachi.munoz@outlook.com>
2023-04-03 23:50:18 -0300

    soy campeon mundial

---
+++ js/nsnc.js
@@ -0,0 +1,14 @@
+/* var ruta = "../home.html";
+
+console.log(window);
+
+if (confirm("¿Eres mayor de edad?")) {
+  window.location.href = ruta;
+} */
+
+var ruta = "../home.html";
+
+console.log(window);
+if (confirm("¿Eres mayor de edad?")) {
+  window.location.href = ruta;
+}
