11730357f3d92ab915e6cba52d371af9907525c9
nachimunoz <nachi.munoz@outlook.com>
2023-04-04 22:42:51 -0300

    y colorin colorado

--- js/nsnc.js
+++ js/nsnc.js
@@ -1,12 +1,4 @@
-/* var ruta = "../home.html";
-
-console.log(window);
-
-if (confirm("¿Eres mayor de edad?")) {
-  window.location.href = ruta;
-} */
-
-var ruta = "../home.html";
+const ruta = "./home.html";
 
 console.log(window);
 if (confirm("¿Eres mayor de edad?")) {
